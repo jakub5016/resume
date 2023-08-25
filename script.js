80f793b006ace0adfb8e039f0d890d9bb67f45bc
jakub5016 <me.jakub.gil@gmail.com>
2023-08-25 14:20:16 +0200

    Add listener to resize window

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const enemies = document.getElementsByClassName("enemy1")
 const game = document.querySelector(".window");
+const player = document.querySelector("character")
 
-// let enemiesWidth = enemies[0].clientWidth
-// console.log(enemies[0].style.left, enemiesWidth, winWidth)
+function setUpEnemies(){
+    const enemyTop = enemies[0].clientTop; // Pobierz początkową wartość top
+    const enemyWidth = 0.06 * window.innerWidth + "px"
+    const enemyHeight = 0.08* window.innerHeight + "px"
 
-enemies[0].style.width = 0.06 * window.innerWidth + "px"
-enemies[0].style.height = 0.08* window.innerHeight + "px"
-enemies[0].style.left = game.clientLeft*1.5 +10+ "px" // 10 is a padding size
-
-const enemyTop = enemies[0].clientTop; // Pobierz początkową wartość top
-const enemyWidth = 0.06 * window.innerWidth + "px"
-const enemyHeight = 0.08* window.innerHeight + "px"
-
-for (let index = 0; index < enemies.length; index++) {
-    enemies[index].style.width = enemyWidth
-    enemies[index].style.height = enemyHeight
-    enemies[index].style.left = game.clientLeft*1.5 +(enemies[0].clientWidth * 2* index)+ "px"
-    enemies[index].style.top = enemyTop -  (enemies[index].clientHeight * index) + "px"
-    enemies[index].style.animation = "moveAnimation 5s infinite"
+    for (let index = 0; index < enemies.length; index++) {
+        enemies[index].style.width = enemyWidth
+        enemies[index].style.height = enemyHeight
+        enemies[index].style.left = game.clientLeft*1.5 +(enemies[0].clientWidth * 2* index)+ "px"
+        enemies[index].style.top = enemyTop -  (enemies[index].clientHeight * index) + "px"
+        enemies[index].style.animation = "moveAnimation 5s infinite"
+    }
 }
+
+setUpEnemies();
+
+window.addEventListener('resize', setUpEnemies);
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+        player.style.animation = "moveAnimation 1s"
+    }
+  });
