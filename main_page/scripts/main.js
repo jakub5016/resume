dee1ceb83e632f76b5793da6b795543db245642e
jakub5016 <me.jakub.gil@gmail.com>
2023-09-04 21:38:10 +0200

    Dots in about me

--- main_page/scripts/main.js
+++ main_page/scripts/main.js
@@ -9,3 +9,9 @@ const observer = new IntersectionObserver((entries)=>{
 
 const hiddenElements = document.querySelectorAll(".container");
 hiddenElements.forEach((elem) => observer.observe(elem))
+
+
+const dots_div = document.getElementsByClassName("dots_image_div")
+const dots_img =document.getElementsByClassName("dots_image")
+console.log(dots_img[0].offsetWidth)
+dots_div[0].style.maxWidth = dots_img[0].offsetWidth +"px"
